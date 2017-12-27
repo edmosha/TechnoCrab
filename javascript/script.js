e30521a9436771dd9362b3a606f6b795db6dc4f3
edmosha <edmosha@github.com>
2017-12-27 15:44:15 +0300

    fix min.js

--- javascript/script.js
+++ javascript/script.js
@@ -5,19 +5,9 @@ var mapBlock = document.querySelector('.map-modal');
 var map = document.querySelector('#map');
 var mapClose = document.querySelector('.close-map');
 
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-    modal.classList.toggle('open');
-});
-
-closeModal.addEventListener('click', function() {
-    modal.classList.remove('open');
-});
-
-var map;
 function initMap() {
         var cord = {lat: 57.7891203, lng: 40.9395435};
-        var balun = 'https://psv4.userapi.com/c816727/u151405344/docs/426efa52037a/balun.png?extra=gUpgZ33COkvaXFEh_FkRvO4nvXhv8f20ZWTOHbzPHUWNRPKbLP4BOoSBKj9hqZlAzr4AAA5O6TvlhAfzuOuiRvoKr53lD9vIWvFMbYf94JjUJPD87uhuNLy9L_I57Id_0eS0WIajdQ'
+        var balun = 'https://psv4.userapi.com/c816727/u151405344/docs/426efa52037a/balun.png?extra=gUpgZ33COkvaXFEh_FkRvO4nvXhv8f20ZWTOHbzPHUWNRPKbLP4BOoSBKj9hqZlAzr4AAA5O6TvlhAfzuOuiRvoKr53lD9vIWvFMbYf94JjUJPD87uhuNLy9L_I57Id_0eS0WIajdQ';
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: cord
@@ -37,4 +27,13 @@ map.addEventListener('click', function(){
   mapClose.addEventListener('click', function(){
     mapBlock.classList.remove('map-open');
     mapClose.classList.remove('show');
+});
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.toggle('open');
+});
+
+closeModal.addEventListener('click', function() {
+    modal.classList.remove('open');
 });

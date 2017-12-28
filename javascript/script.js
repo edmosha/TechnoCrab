b6b81e95b0acb652e2ec86d8334d8f9f2f61de28
edmosha <edmosha@github.com>
2017-12-28 20:34:55 +0300

    fix balun

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,7 @@ var mapClose = document.querySelector('.close-map');
 
 function initMap() {
         var cord = {lat: 57.7891203, lng: 40.9395435};
-        var balun = 'https://psv4.userapi.com/c816727/u151405344/docs/426efa52037a/balun.png?extra=gUpgZ33COkvaXFEh_FkRvO4nvXhv8f20ZWTOHbzPHUWNRPKbLP4BOoSBKj9hqZlAzr4AAA5O6TvlhAfzuOuiRvoKr53lD9vIWvFMbYf94JjUJPD87uhuNLy9L_I57Id_0eS0WIajdQ';
+        var balun = 'https://image.ibb.co/eERm1b/image.png';
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: cord
